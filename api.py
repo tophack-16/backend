2b08be775004e003f874773b2f6e261dca84febf
zhanghuimeng <zhanghuimeng1997@gmail.com>
2019-03-24 01:53:17 +0800

    增加了更多API，现在输出是我们的，而且是合法的

--- api.py
+++ api.py
@@ -11,21 +11,35 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
-parser = reqparse.RequestParser()
-parser.add_argument('class', type=list)
-parser.add_argument('round', type=int, default=None, required=True, help='Round')
-parser.add_argument('turn', type=int, default=None, required=True, help='Turn')
+board_parser = reqparse.RequestParser()
+board_parser.add_argument('round', type=int, default=None, required=True, help='Round')
+board_parser.add_argument('turn', type=int, default=None, required=True, help='Turn')
 
 class Board(Resource):
     def get(self):
-        args = parser.parse_args()
-        round = args['round']  # List ['A', 'B']
-        turn = args['turn']  # Boolean True
+        args = board_parser.parse_args()
+        round = args['round']
+        turn = args['turn']
         with open("output/board_round_%d_turn_%d.json" % (round, turn), 'r') as f:
             board = json.load(f)
         return board
 
 api.add_resource(Board, '/board')
+
+action_parser = reqparse.RequestParser()
+action_parser.add_argument('round', type=int, default=None, required=True, help='Round')
+action_parser.add_argument('turn', type=int, default=None, required=True, help='Turn')
+
+class Action(Resource):
+    def get(self):
+        args = action_parser.parse_args()
+        round = args['round']
+        turn = args['turn']
+        with open("output/action_round_%d_turn_%d.json" % (round, turn), 'r') as f:
+            action = json.load(f)
+        return action
+
+api.add_resource(Action, '/action')
 
 if __name__ == '__main__':
     app.run(debug=True)
